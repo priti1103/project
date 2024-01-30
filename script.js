f21cca02a3e4459c417367ebda6412a91d90cd08
priti1103 <158147101+priti1103@users.noreply.github.com>
2024-01-30 14:35:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const dropdownbtn =document.querySelectorAll(".dropdownbtn")
+const dropdownbox =document.querySelectorAll(".dropdownbox")
+
+dropdownbtn.forEach((dropb,index)=>{
+    dropb.addEventListener("mouseover",()=>{
+
+        dropdownbox[index].classList.remove("active")
+    })
+    dropb.addEventListener("mouseout",()=>{
+
+        dropdownbox[index].classList.add("active")
+    })
+})
